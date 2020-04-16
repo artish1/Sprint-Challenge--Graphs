0755d54153d8669d39add5d6fc13704829abe80f
Mark Artishuk <markyshuk@gmail.com>
2020-04-16 12:53:13 -0700

    Created custom graph for handling room edges and connections

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from graph import Graph
 
 import random
 from ast import literal_eval
@@ -10,14 +11,14 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,11 +26,36 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+# Graph
+
+graph = Graph()
+graph.add_room(player.current_room)
+
+prev_room = player.current_room
+player.travel("n")
+graph.add_room(player.current_room)
+graph.connect_room(prev_room, player.current_room, "n")
+
+graph.print_rooms()
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
 
+# (Base Case ?) If room has no unexplored paths, walk back to nearest room that does contain an unexplored (?) path.
+# BFS ^
+
+# (Base case ?) If BFS cannot find a room that has unexplored paths, it's done.
+
+# Pick random unexplored direction from player's current room.
+unexplored_directions = []
+
+
+# Travel
+
+# Log direction
+
+# Loop ?
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -41,22 +67,23 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited"
+    )
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
---
+++ graph.py
@@ -0,0 +1,36 @@
+
+
+class Graph:
+
+    def __init__(self):
+        self.rooms = {}
+        self.opposite_directions = {
+            'n': 's',
+            's': 'n',
+            'e': 'w',
+            'w': 'e'
+        }
+
+    def add_room(self, room):
+        room_info = {}
+        for direction in room.get_exits():
+            room_info[direction] = "?"
+
+        self.rooms[room.id] = room_info
+
+    def connect_room(self, from_room, to_room, direction):
+        if self.rooms[from_room.id] is None:
+            return print("Cannot connect a room that has not been added to the group initially")
+        
+        if self.rooms[from_room.id][direction] is None:
+            return print(f"Room {from_room.id} does not have a {direction} direction!")
+        
+        self.rooms[from_room.id][direction] = to_room.id
+        opposite_dir = self.opposite_directions[direction]
+        self.rooms[to_room.id][opposite_dir] = from_room.id
+
+
+    def print_rooms(self):
+        print(self.rooms)
+
+    
